2403c6626d4a759699c985f023fc3fde66868529
Eric Bandin <ericmbandin@gmail.com>
2019-10-09 00:16:16 -0400

    build a blog almost done

---
+++ main.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, redirect, render_template, session
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://MyBloggy:MyBloggy@localhost:8889/MyBloggy'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+
+
+class MyBloggy(db.Model):
+    title = db.Column(db.String(50), primary_key=True)
+    blog = db.Column(db.String(500))
+    
+    
+
+    def __init__(self, title, blog):
+        self.title = title
+        self.blog = blog
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        title = request.form['title']
+        blog = request.form['blog']
+        new_post = MyBloggy(title, blog)
+        db.session.add(new_post)
+        db.session.commit()
+    
+    completed_title = MyBloggy.query.all()
+
+    return render_template('blog.html', completed_title = completed_title)
+
+@app.route('/postblog', methods=['POST', 'GET']) 
+def postblog(): 
+
+        
+    return render_template('postblog.html')
+
+@app.route('/blog-post', methods=['POST', 'GET']) 
+def redirectblog(): 
+    #need to find a way to request the title and post data, so that I can format it into blog-post.html
+    return render_template('blog-post.html' )
+
+
+if __name__ == '__main__':
+    app.run()
